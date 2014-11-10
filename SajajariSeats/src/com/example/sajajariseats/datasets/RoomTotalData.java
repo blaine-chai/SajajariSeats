7e09385ee241d6e36b1104569960cfc9f53cb78c
blainechai <git clone https://Blainechai@bitbucket.org/Blainechai/sajajariseats.git>
2014-11-10 15:45:09 +0900

    하잇

---
+++ SajajariSeats/src/com/example/sajajariseats/datasets/RoomTotalData.java
@@ -0,0 +1,41 @@
+package com.example.sajajariseats.datasets;
+
+public class RoomTotalData {
+	private int seatNumMax;
+	private int filled;
+	private int empty;
+	private double percent;
+
+	public int getSeatNumMax() {
+		return seatNumMax;
+	}
+
+	public void setSeatNumMax(int seatNumMax) {
+		this.seatNumMax = seatNumMax;
+	}
+
+	public int getFilled() {
+		return filled;
+	}
+
+	public void setFilled(int filled) {
+		this.filled = filled;
+	}
+
+	public int getEmpty() {
+		return empty;
+	}
+
+	public void setEmpty(int empty) {
+		this.empty = empty;
+	}
+
+	public double getPercent() {
+		return percent;
+	}
+
+	public void setPercent(double percent) {
+		this.percent = percent;
+	}
+
+}
